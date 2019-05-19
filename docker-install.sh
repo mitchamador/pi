e815b14f9afa96ba4e22494108decf65517eefc8
mitchamador <vicokenator@gmail.com>
2019-05-19 15:26:22 +0300

    Create docker-install.sh

---
+++ docker-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt update && sudo apt -y upgrade
+
+id=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
+
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/$id/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=armhf] https://download.docker.com/linux/$id \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce
