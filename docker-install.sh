dabd417e46738e23d54ded49e952e7bbe9b893f1
michamador <vicokenator@gmail.com>
2023-08-22 19:54:49 +0000

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-sudo apt update && sudo apt -y upgrade
-
 id=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
 
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common gnupg
-curl -fsSL https://download.docker.com/linux/$id/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=armhf] https://download.docker.com/linux/$id \
-   $(lsb_release -cs) \
-   stable"
 sudo apt-get update
-sudo apt-get -y install docker-ce
+sudo apt-get install -y ca-certificates curl gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/$id/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$id \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
